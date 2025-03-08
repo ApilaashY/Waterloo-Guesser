25d910d3438753ebf62ec1e6af0f6de50467920c
TheOneAndOnlyAppleMan <apilaashyoharan1@gmail.com>
2025-03-08 10:57:28 -0500

    server start setup

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,43 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const fs = require("node:fs");
+const { config } = require("dotenv");
+var mongo = require("mongodb").MongoClient;
+require("dotenv/config");
+
+config();
+
+const app = express();
+
+mongo.connect(
+  "mongodb+srv://Apilaash:Dinosaurs@16@cluster1.v3kqu.mongodb.net/?",
+  function (err, db) {
+    if (err) throw err;
+
+    console.log(db);
+
+    db.close();
+  }
+);
+
 app.use(cors());
+
+const folderName = "photos";
+const photos = fs.readdirSync(folderName);
 
 app.get("/", (req, res) => {
   res.send({ Done: "There" });
+});
+
+app.get("/getPhoto", (req, res) => {
+  fs.readFile("photos/" + photos[0], "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    res.contentType("image/jpeg"); // Set content type for image
+    res.json({ data });
+  });
 });
 
 const PORT = process.env.PORT || 8080;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,15 @@ export default function App() {
   const [state, setState] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:8080/")
+    fetch("http://localhost:8080/getPhoto")
       .then((res) => res.json())
-      .then((json) => setState(json["Done"]));
+      .then((json) => setState(json["Photo"]));
   }, []);
 
-  return <h1>Hello {state}</h1>;
+  return (
+    <>
+      <h1>Hello</h1>
+      <img content={state} />
+    </>
+  );
 }
