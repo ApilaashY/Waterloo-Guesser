79e5ceb07a5de8f7eec5df284fbac911ad869a00
TheOneAndOnlyAppleMan <apilaashyoharan1@gmail.com>
2025-03-31 23:48:06 -0400

    finally centered HI

--- frontend/src/Map/Map.tsx
+++ frontend/src/Map/Map.tsx
@@ -5,16 +5,22 @@ export default function Map() {
   const [xCoor, setXCoor] = useState(0);
   const [yCoor, setYCoor] = useState(0);
   function handleClick(event: React.MouseEvent<HTMLImageElement>) {
-    const x = event.clientX - event.currentTarget.offsetLeft;
-    const y = event.clientY - event.currentTarget.offsetTop;
-    console.log(`Clicked at coordinates: (${x}, ${y})`);
+    const x =
+      event.pageX - (event.currentTarget.parentElement?.offsetLeft ?? 0);
+    const y = event.pageY - (event.currentTarget.parentElement?.offsetTop ?? 0);
+    console.log(
+      `Clicked at coordinates: (${x}, ${y}) (${event.clientX}, ${
+        event.clientY
+      }) (${event.currentTarget.parentElement?.offsetLeft ?? 0}, ${
+        event.currentTarget.parentElement?.offsetTop ?? 0
+      })`
+    );
     setXCoor(x);
     setYCoor(y);
   }
 
   return (
     <div>
-      <div className="Box"></div>
       <div className="Box">
         <img src="campus-map.png" alt="Campus Map" onClick={handleClick} />
         <div className="InnerBox" style={{ top: yCoor, left: xCoor }}>
