168420c8f260e945450ef6c5e3bddce8905966bc
TheOneAndOnlyAppleMan <apilaashyoharan1@gmail.com>
2025-04-22 21:41:43 -0400

    s

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -48,7 +48,7 @@ export default function App() {
   >(null);
 
   function requestImage() {
-    fetch(`/api/getPhoto?previousCode=${imageID}`)
+    fetch("/api/getPhoto?previousCode=" + imageID)
       .then((res) => res.json())
       .then((json) => {
         setImageID(json.id);
@@ -61,7 +61,7 @@ export default function App() {
   }
 
   function validateCoordinate() {
-    fetch(`/api/validateCoordinate`, {
+    fetch("/api/validateCoordinate", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
