0b0579cf5f317876db602d68b04ee75b088885cb
TheOneAndOnlyAppleMan <apilaashyoharan1@gmail.com>
2025-04-22 21:51:29 -0400

    links

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App/App.tsx";
 
 export const host = import.meta.env.VITE_HOST; // Use VITE_HOST
-console.log("Host: ", host);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import * as dotenv from "dotenv";
-
-dotenv.config();
-
-const host = process.env.VITE_HOST;
-console.log(`VITE_HOST: ${host}`);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: host, // Your backend URL
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ""),
-  //     },
-  //   },
-  // },
 });
