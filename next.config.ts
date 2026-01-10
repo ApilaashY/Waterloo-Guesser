7dfb0a473e47c70c85b1b5eea370056b971656c3
Apilaash <apilaashyoharan1@gmail.com>
2026-01-09 22:33:17 -0500

    turbopack fixes

--- next.config.ts
+++ next.config.ts
@@ -4,20 +4,11 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: false,
   },
-  webpack: (config, { isServer }) => {
-    // Exclude test files from webpack compilation
-    config.module.rules.push({
-      test: /\/(vitest|playwright)\.config\.ts$/,
-      use: 'ignore-loader'
-    });
-    
-    return config;
-  },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
       },
     ],
   },
