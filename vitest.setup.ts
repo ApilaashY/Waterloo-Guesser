7d7eea433310f83a4f9012bcdadd0dc6ee47d455
Apilaash <apilaashyoharan1@gmail.com>
2026-01-09 22:36:35 -0500

    readd tests

---
+++ vitest.setup.ts
@@ -0,0 +1,7 @@
+import "@testing-library/jest-dom";
+import createFetchMock from "vitest-fetch-mock";
+import { vi } from "vitest";
+
+const fetchMock = createFetchMock(vi);
+
+fetchMock.enableMocks();
